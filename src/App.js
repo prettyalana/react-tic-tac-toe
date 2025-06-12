95b931e9122b46f45bab507b903d220ec087b0fc
prettyalana <129349619+prettyalana@users.noreply.github.com>
2025-06-11 20:57:50 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+export default function Board() {
+  return (
+  <>
+  <div className="board-row">
+    <Square/>
+    <Square/>
+    <Square/>
+  </div>
+  <div className="board-row">
+    <Square/>
+    <Square/>
+    <Square/>
+  </div>
+  <div className="board-row">
+    <Square/>
+    <Square/>
+    <Square/>
+  </div>
+  </>
+  );
+}
+ 
+function Square() {
+  return <button className="square"></button>
+}
